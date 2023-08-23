6d4626c4ed3f3543060131f2a99784752c77264b
bbkx226 <bbkx226@gmail.com>
2023-08-23 09:46:25 +0800

    Create Graphic Template

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
 def main():
-    pass
+    st.set_page_config(page_title="PDFPal", page_icon=":books:")
 
-if __name__ == '__main__':
+    st.header("Chat with multiple PDFs at once :books:")
+    st.text_input("Ask question about your documents:")
+
+    with st.sidebar:
+        st.subheader('Your documents')
+        st.file_uploader("Upload your PDFs here and click on Process")
+        st.button("Process")
+
+if __name__ == '__main__': # define code that should only run when the script is executed directly
     main()
