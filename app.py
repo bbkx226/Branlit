1907202a8265617a8864628b4c169916d029e038
bbkx226 <bbkx226@gmail.com>
2023-08-23 10:45:47 +0800

    Build conversation between user and AI

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
+from htmlTemplates import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -43,15 +44,32 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i & 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="PDFPal", page_icon=":books:")
 
+    st.write(css, unsafe_allow_html=True)
+
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
     st.header("Chat with multiple PDFs at once :books:")
-    st.text_input("Ask question about your documents:")
+    user_question = st.text_input("Ask question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
 
     with st.sidebar:
         st.subheader('Your documents')
@@ -60,11 +78,9 @@ def main():
             with st.spinner("Processing your documents..."):
                 # Get PDF text
                 raw_text = get_pdf_text(pdf_docs)
-                st.write(raw_text)
 
                 # Get the text chunks
                 text_chunks = get_text_chunks(raw_text)
-                st.write(text_chunks) 
 
                 # Create Vector Store
                 vectorstore = get_vectorstore(text_chunks)
