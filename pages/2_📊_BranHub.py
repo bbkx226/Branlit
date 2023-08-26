9370c3aad6ef44ab9046e94061a4c9aabda4df0e
bbkx226 <bbkx226@gmail.com>
2023-08-26 15:53:12 +0800

    Fix Bugs

--- pages/2_📊_BranHub.py
+++ pages/2_📊_BranHub.py
@@ -7,22 +7,27 @@ from pandasai.llm.openai import OpenAI
 import matplotlib
 import matplotlib.pyplot as plt
 import warnings
+import glob  # Import glob to work with file paths
+import os.path  # Import os.path to handle file paths
 
 warnings.filterwarnings("ignore")
 
 def main():
-    
     load_dotenv()
-    matplotlib.use('Agg')
+    matplotlib.use('TkAgg')
     API_KEY = os.environ["OPENAI_API_KEY"]
     llm = OpenAI(api_token=API_KEY)
-    pandas_ai = PandasAI(llm)
+    pandas_ai = PandasAI(llm, save_charts=True)
     st.set_option('deprecation.showPyplotGlobalUse', False)
     st.set_page_config(page_title="BranHub", page_icon="📊")
     st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
     st.title("📊 BranHub: The AI Data Analyst")
     st.header("PDFs In, Answers Out! ⚡")
     st.write("""---""") 
+
+    # Define your file path and folder
+    graph_folder_path = "./exports/charts/"  # Update with the correct folder path
+    count_files = len(glob.glob(os.path.join(graph_folder_path, '*.png')))  # Count the PNG files
 
     with open("./files/covid-data.csv", "rb") as file:
         st.download_button(
@@ -37,23 +42,27 @@ def main():
     uploaded_file = st.file_uploader(":file_folder: Upload a CSV file for analysis", type=['csv'])
     
     if uploaded_file is not None:
-        with st.spinner("Uploading CSV..."):
-            df = pd.read_csv(uploaded_file)
-            st.write(df.head(5))
-            st.write("""---""") 
-            prompt = st.text_area("Enter your prompt:")
+        df = pd.read_csv(uploaded_file)
+        st.write(df.head(5))
+        st.write("""---""") 
+        prompt = st.text_area("Enter your prompt:")
 
-            if st.button("Generate"):
-                if prompt:
-                    with st.spinner("Generating response..."):
-                        answer = pandas_ai.run(df,prompt)
-                        fig_number = plt.get_fignums()
-                        if fig_number:
-                            st.pyplot()
-                        else:
-                            st.write(answer)
-                else:
-                    st.warning("Please enter a prompt.")
+        if st.button("Generate"):
+            if prompt:
+                with st.spinner("Generating response..."):
+                    answer = pandas_ai.run(df,prompt)
+                    current_file_count = len(glob.glob(os.path.join(graph_folder_path, '*.png')))  # Count the PNG files again
+                    if current_file_count > count_files:
+                        count_files = current_file_count
+                        st.write(answer)
+                        list_of_files = glob.glob(os.path.join(graph_folder_path, '*.png')) 
+                        st.write(list_of_files)
+                        latest_file = max(list_of_files, key=os.path.getctime)
+                        st.image(latest_file)
+                    else:
+                        st.write(answer)
+            else:
+                st.warning("Please enter a prompt.")
                 
 if __name__ == '__main__':
     main()
