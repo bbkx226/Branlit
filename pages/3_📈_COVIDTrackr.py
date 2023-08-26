5298ec42dd061d0d73721fe2acb78c6f6413ceef
bbkx226 <bbkx226@gmail.com>
2023-08-26 14:36:02 +0800

    Fixed Bugs - Hierarchical Map

--- pages/3_📈_COVIDTrackr.py
+++ pages/3_📈_COVIDTrackr.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import plotly.express as px
-import plotly.figure_factory as ff
 import pandas as pd
-import os
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -12,15 +10,15 @@ def main():
     st.set_page_config(page_title="COVIDTrackr", page_icon="📈", layout="wide")
     st.title(" 📈 COVIDTrackr")
     st.header("Together, We Analyze, Together, We Defeat. 🔬")
-    st.write("""---""") 
-    st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
+    st.write("""---""")
+    st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 
-    df = pd.read_csv("./files/covid-data.csv", encoding = "ISO-8859-1")
+    df = pd.read_csv("./files/covid-data.csv", encoding="ISO-8859-1")
 
     col1, col2 = st.columns((2))
     df["date"] = pd.to_datetime(df["date"])
 
-    # Getting the min and max date 
+    # Getting the min and max date
     startDate = pd.to_datetime(df["date"]).min()
     endDate = pd.to_datetime(df["date"]).max()
 
@@ -38,7 +36,7 @@ def main():
     st.sidebar.header("Choose your filter: ")
     continent = st.sidebar.multiselect("Pick Your Continent", df["continent"].unique())
     if not continent:
-        df2 =df.copy()
+        df2 = df.copy()
     else:
         df2 = df[df["continent"].isin(continent)].copy()
 
@@ -55,10 +53,10 @@ def main():
     elif not location:
         filtered_df = df[df["continent"].isin(continent)]
     else:
-        filtered_df = df3[df3["continent"].isin(continent) & df3['location'].isin(location)] 
+        filtered_df = df3[df3["continent"].isin(continent) & df3['location'].isin(location)]
 
     # Filter out null continents
-    filtered_df = filtered_df[filtered_df['continent'].notnull()] 
+    filtered_df = filtered_df[filtered_df['continent'].notnull()]
 
     filtered_df['month_year'] = filtered_df['date'].dt.to_period('M')
 
@@ -68,17 +66,24 @@ def main():
     end_location_df['total_cases'] = end_location_df['total_cases'].fillna(0)
     start_location_df['total_cases'] = start_location_df['total_cases'].fillna(0)
 
+    end_location_df['total_deaths'] = end_location_df['total_deaths'].fillna(0)
+    start_location_df['total_deaths'] = start_location_df['total_deaths'].fillna(0)
+
     end_location_df = end_location_df.reset_index(drop=True)
     start_location_df = start_location_df.reset_index(drop=True)
 
     location_df = pd.merge(end_location_df, start_location_df, on='location')
 
-    location_df['total_cases_end'] = location_df['total_cases_x'] 
+    location_df['total_cases_end'] = location_df['total_cases_x']
     location_df['total_cases_start'] = location_df['total_cases_y']
     location_df['total_cases_change'] = location_df['total_cases_end'] - location_df['total_cases_start']
 
+    location_df['total_deaths_end'] = location_df['total_deaths_x']
+    location_df['total_deaths_start'] = location_df['total_deaths_y']
+    location_df['total_deaths_change'] = location_df['total_deaths_end'] - location_df['total_deaths_start']
+
     st.subheader("Location wise Cases")
-    fig = px.bar(location_df, x = "location", y = "total_cases_change", color = "location", template="seaborn")
+    fig = px.bar(location_df, x="location", y="total_cases_change", color="location", template="seaborn")
     st.plotly_chart(fig, use_container_width=True, height=200)
 
     # Group by continent and date to sum up the total cases for each country
@@ -98,27 +103,55 @@ def main():
 
     continent_df = pd.merge(end_continent_df, start_continent_df, on='continent')
 
-    continent_df['total_cases_end'] = continent_df['total_cases_x'] 
+    continent_df['total_cases_end'] = continent_df['total_cases_x']
     continent_df['total_cases_start'] = continent_df['total_cases_y']
     continent_df['total_cases_change'] = continent_df['total_cases_end'] - continent_df['total_cases_start']
 
     st.subheader("Continent wise Cases")
-    fig = px.pie(continent_df, values = "total_cases_change", names = "continent", hole = 0.5)
-    fig.update_traces(text = continent_df["continent"], textposition = "outside")
-    st.plotly_chart(fig,use_container_width=True)
+    fig = px.pie(continent_df, values="total_cases_change", names="continent", hole=0.5)
+    fig.update_traces(text=continent_df["continent"], textposition="outside")
+    st.plotly_chart(fig, use_container_width=True)
 
     st.subheader('Time Series Analysis')
     monthly_cases = filtered_df.groupby(['month_year', 'location'])['total_cases'].last().reset_index()
     linechart = pd.DataFrame(monthly_cases.groupby(monthly_cases["month_year"].dt.strftime("%Y : %b"))["total_cases"].sum()).reset_index()
-    linechart = linechart.sort_values(by = "total_cases")
-    fig2 = px.line(linechart, x = "month_year", y="total_cases", labels = {"total_cases": "Cases"},height=500, width = 1000,template="gridon")
-    st.plotly_chart(fig2,use_container_width=True)    
+    linechart = linechart.sort_values(by="total_cases")
+    fig2 = px.line(linechart, x="month_year", y="total_cases", labels={"total_cases": "Cases"}, height=500, width=1000, template="gridon")
+    st.plotly_chart(fig2, use_container_width=True)
+
+    end_location_df = filtered_df.groupby(['continent', 'location']).apply(lambda group: group[group['date'] == group['date'].max()])
+    start_location_df = filtered_df.groupby(['continent', 'location']).apply(lambda group: group[group['date'] == group['date'].min()])
+
+    end_location_df['total_cases'] = end_location_df['total_cases'].fillna(0)
+    start_location_df['total_cases'] = start_location_df['total_cases'].fillna(0)
+
+    end_location_df['total_deaths'] = end_location_df['total_deaths'].fillna(0)
+    start_location_df['total_deaths'] = start_location_df['total_deaths'].fillna(0)
+
+    end_location_df = end_location_df.reset_index(drop=True)
+    start_location_df = start_location_df.reset_index(drop=True)
+
+    location_df = pd.merge(end_location_df, start_location_df, on=['continent', 'location'])
+
+    location_df['total_cases_end'] = location_df['total_cases_x']
+    location_df['total_cases_start'] = location_df['total_cases_y']
+    location_df['total_cases_change'] = abs(location_df['total_cases_end'] - location_df['total_cases_start'])
+
+    location_df['total_deaths_end'] = location_df['total_deaths_x']
+    location_df['total_deaths_start'] = location_df['total_deaths_y']
+    location_df['total_deaths_change'] = abs(location_df['total_deaths_end'] - location_df['total_deaths_start'])
 
     st.subheader("Hierarchical view of Cases using TreeMap")
-    fig3 = px.treemap(location_df, path = ["continent","location", "total_deaths"], values = "total_deaths", hover_data = ["total_deaths"],
-                    color = "total_cases")
-    fig3.update_layout(width = 800, height = 650)
+    # Group by continent and location to sum up the total deaths and total cases for each location
+    location_total_cases = location_df.groupby(['continent', 'location'])[['total_deaths_change', 'total_cases_change']].sum().reset_index()
+    # Filter out rows with zero values
+    location_total_cases = location_total_cases[(location_total_cases['total_deaths_change'] != 0) & (location_total_cases['total_cases_change'] != 0)]
+
+    fig3 = px.treemap(location_total_cases, path=["continent", "location"], values="total_deaths_change",
+                    hover_data=["total_deaths_change", "total_cases_change"], color="total_cases_change")
+
+    fig3.update_layout(width=800, height=650)
     st.plotly_chart(fig3, use_container_width=True)
 
-if __name__ == '__main__': # define code that should only run when the script is executed directly
+if __name__ == '__main__':  # define code that should only run when the script is executed directly
     main()
